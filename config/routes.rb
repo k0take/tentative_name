04b1d3e310cba3adae7905f8f88d48f93bbad194
kotake <akimotorina10@gmail.com>
2023-01-10 17:05:01 +0900

    Add: ユーザーの新規登録機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'static_pages#top'
+  resources :users, only: %i[new create]
 end
