500e93c1a2c7f180308929eb968ce5011513a881
kotake <akimotorina10@gmail.com>
2023-01-10 17:23:23 +0900

    Fix: =の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path, notice: = 'ユーザー登録が完了しました'
+      redirect_to new_user_path, notice: 'ユーザー登録が完了しました'
     else
       flash[:alert] = 'ユーザー登録ができませんでした'
       render :new
