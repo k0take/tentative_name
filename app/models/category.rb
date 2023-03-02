80c868f158967f244692454849c85e7555f4fece
kotake <akimotorina10@gmail.com>
2023-03-02 19:45:23 +0900

    Fix: カテゴリーモデルの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-  has_many :product_categories, dependent: :destroy
-  has_many :categories, through: :product_categories, dependent: :destroy
+  has_many :product_categories, dependent: :destroy, foreign_key: 'category_id'
+  has_many :products, through: :product_categories
 
   validates :category_name, uniqueness: true
 end
