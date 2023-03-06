ee36e9437231bcaf2005a99e6ec0b811c45af72a
kotake <akimotorina10@gmail.com>
2023-03-06 20:21:59 +0900

    Add: カテゴリー検索機能の追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ApplicationRecord
   has_many :products, through: :product_categories
 
   validates :category_name, uniqueness: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["category_name", "created_at", "id", "updated_at"]
+  end
 end
