db1b6798ebf914d52f2fd1b52f47a528c4d81596
kotake <akimotorina10@gmail.com>
2023-03-07 12:40:52 +0900

    Fix: 一覧表示の修正

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,6 +2,8 @@ class Store < ApplicationRecord
   has_many :product_stores
   has_many :products, through: :product_stores
 
+  validates :name, uniqueness: true, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "id", "name", "updated_at"]
   end
